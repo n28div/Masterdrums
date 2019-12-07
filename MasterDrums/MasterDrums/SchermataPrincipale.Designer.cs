e869c20638d21e05738dd7ba3e2c240c13ac7ffc
Matteo Di Lorenzi <matteo.dilorenzi98@gmail.com>
2019-12-07 14:10:02 +0100

    Added VS project folder

---
+++ MasterDrums/MasterDrums/SchermataPrincipale.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace MasterDrums
+{
+    partial class SchermataPrincipale
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNuovaPartita = new System.Windows.Forms.Button();
+            this.lblNomeGiocatore = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnNuovaPartita
+            // 
+            this.btnNuovaPartita.Location = new System.Drawing.Point(340, 181);
+            this.btnNuovaPartita.Name = "btnNuovaPartita";
+            this.btnNuovaPartita.Size = new System.Drawing.Size(75, 23);
+            this.btnNuovaPartita.TabIndex = 0;
+            this.btnNuovaPartita.Text = "NUOVA PARTITA";
+            this.btnNuovaPartita.UseVisualStyleBackColor = true;
+            // 
+            // lblNomeGiocatore
+            // 
+            this.lblNomeGiocatore.AutoSize = true;
+            this.lblNomeGiocatore.Location = new System.Drawing.Point(312, 286);
+            this.lblNomeGiocatore.Name = "lblNomeGiocatore";
+            this.lblNomeGiocatore.Size = new System.Drawing.Size(132, 17);
+            this.lblNomeGiocatore.TabIndex = 1;
+            this.lblNomeGiocatore.Text = "Inserisci il tuo nome";
+            // 
+            // SchermataPrincipale
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblNomeGiocatore);
+            this.Controls.Add(this.btnNuovaPartita);
+            this.Name = "SchermataPrincipale";
+            this.Text = "MasterDrums";
+            this.Load += new System.EventHandler(this.SchermataPrincipale_Load);
+            this.Resize += new System.EventHandler(this.SchermataPrincipale_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNuovaPartita;
+        private System.Windows.Forms.Label lblNomeGiocatore;
+    }
+}
+
---
+++ MasterDrums/MasterDrums/SchermataPrincipale.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MasterDrums
+{
+    public partial class SchermataPrincipale : Form
+    {
+
+        // nome giocatore
+        private String _nomeGiocatore;
+
+        public SchermataPrincipale()
+        {
+            InitializeComponent();
+        }
+
+        // caricamento iniziale della finestra
+        private void SchermataPrincipale_Load(object sender, EventArgs e)
+        {
+            // definizione grandezza minima della finestra
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+
+            // visualizzazione lblNomeGiocatore
+            lblNomeGiocatore.Visible = true;
+            lblNomeGiocatore.TextAlign = ContentAlignment.MiddleCenter;
+            lblNomeGiocatore.BackColor = Color.Red;
+
+            // bottone nuova partita
+            btnNuovaPartita.Size = new Size((this.Width / 7), (this.Height / 7));
+            btnNuovaPartita.Location = new Point((this.Width / 2 - btnNuovaPartita.Width / 2), (this.Height / 2 - btnNuovaPartita.Height / 2) - (this.Height / 10));
+            btnNuovaPartita.Font = new Font(btnNuovaPartita.Font.FontFamily, (this.Width / 100));
+
+            // label nome giocatore
+            lblNomeGiocatore.Size = new Size((this.Width / 6), (this.Height / 6));
+            lblNomeGiocatore.Location = new Point((this.Width / 2 - lblNomeGiocatore.Width / 2), (this.Height / 2 - lblNomeGiocatore.Height / 2) + (this.Height / 10));
+            lblNomeGiocatore.Font = new Font(btnNuovaPartita.Font.FontFamily, 2 * (this.Width / 100));
+        }
+
+        // resize automatico dei componenti nel momento in cui viene effettuato un ridimensionamento della finestra
+        private void SchermataPrincipale_Resize(object sender, EventArgs e)
+        {
+
+                // bottone nuova partita
+                btnNuovaPartita.Size = new Size((this.Width / 6), (this.Height / 7));
+                btnNuovaPartita.Location = new Point((this.Width / 2 - btnNuovaPartita.Width / 2), (this.Height / 2 - btnNuovaPartita.Height / 2) - (this.Height / 10));
+                btnNuovaPartita.Font = new Font(lblNomeGiocatore.Font.FontFamily, (this.Width / 100));
+
+                // label nome giocatore
+                lblNomeGiocatore.Size = new Size((this.Width / 6), (this.Height / 6));
+                lblNomeGiocatore.Location = new Point((this.Width / 2 - lblNomeGiocatore.Width / 2), (this.Height / 2 - lblNomeGiocatore.Height / 2) + (this.Height / 10));
+                lblNomeGiocatore.Font = new Font(lblNomeGiocatore.Font.FontFamily, 2 * (this.Width / 100));
+            
+        }
+
+    }
+}
